f87dc58d90f60e50ef43e48baafe26ddfdd9a91a
Matthew Cadol <link0833@gmail.com>
2019-11-13 12:44:26 -0700

    I want and I _ you statements

--- src/Magpie4.java
+++ src/Magpie4.java
@@ -48,6 +48,10 @@ public class Magpie4 {
 			response = transformIWantToStatement(statement);
 		}
 
+		else if (findKeyword(statement, "I want", 0) >= 0) {
+			response = transformIWantStatement(statement);
+		}
+
 		else {
 			// Look for a two word (you <something> me)
 			// pattern
@@ -55,7 +59,11 @@ public class Magpie4 {
 
 			if (psn >= 0 && findKeyword(statement, "me", psn) >= 0) {
 				response = transformYouMeStatement(statement);
-			} else {
+			}
+            else if (psn >= 0 && findKeyword(statement, "I", 0) >= 0) {
+                response = transformIYouStatement(statement);
+            }
+            else {
 				response = getRandomResponse();
 			}
 		}
@@ -105,6 +113,30 @@ public class Magpie4 {
 				.trim();
 		return "What makes you think that I " + restOfStatement + " you?";
 	}
+
+    private String transformIWantStatement(String statement) {
+        // Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+        int psn = findKeyword(statement, "I want", 0);
+        String restOfStatement = statement.substring(psn + 6).trim();
+        return "Would you really be happy if you had " + restOfStatement + "?";
+    }
+
+    private String transformIYouStatement(String statement) {
+        // Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+        int psn = findKeyword(statement, "you", 0);
+        String restOfStatement = statement.substring(2, psn).trim();
+        return "Why do you " + restOfStatement + " me?";
+    }
 
 	/**
 	 * Search for one word in phrase. The search is not case sensitive. This
